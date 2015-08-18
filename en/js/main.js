f141fe72ad0c0a170de0c8247dd30175e3772db2
Teemuer <teemu.eronen@hotmail.fi>
2015-08-18 12:01:29 +0300

    modal-wide max-height = windowheight

--- en/js/main.js
+++ en/js/main.js
@@ -24,6 +24,12 @@ $(function() {
 	    }, 500);
 	});
 	
+	// when .modal-wide opened, set content-body height based on browser height; 200 is approx height of modal padding, modal title and button bar
+	$(".modal-wide").on("show.bs.modal", function() {
+	  var height = $(window).height() - 200;
+	  $(this).find(".modal-body").css("max-height", height);
+	});
+
 	/*//close all accordions
 	$('.closeall').click(function(){
 		  $('.panel-collapse.in')
